6f9351f30d689a38a46b3e0ecff2c3380f6465c9
jacobwshg <wsg2002@hotmail.com>
2025-07-30 09:29:45 -0500

    upload

---
+++ sbshow.py
@@ -0,0 +1,18 @@
+from sys import argv
+
+# show bytes in character
+def cbshow(c):
+    return f'{c}\t{hex(ord(c))}'
+
+# show bytes in string
+def sbshow(s):
+    return '\n'.join([cbshow(c) for c in s])
+
+def main():
+    for s in argv[1:]:
+        print(f'{sbshow(s)}\n')
+    exit()
+
+if __name__ == '__main__':
+    main()
+
